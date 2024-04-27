67f07d585a903dba672b561037511775d6a4464c
darwin mamani <darwin_xd10@hotmail.com>
2024-04-26 22:10:21 -0400

    mod vector form

--- 02-Vector_Integer/main.pas
+++ 02-Vector_Integer/main.pas
@@ -16,9 +16,9 @@ type
     Button2: TButton;
     Button3: TButton;
     Button4: TButton;
-    Button5: TButton;
     ComboBox1: TComboBox;
     Edit1: TEdit;
+    Label1: TLabel;
     StringGrid1: TStringGrid;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
@@ -30,8 +30,8 @@ type
   private
 
   public
-        v, v1, v2, v3, v4, v5 :vector;
-        VEC: array[0..5] of vector;
+        v :vector;
+        VEC: array[1..5] of vector;
   end;
 
 var
@@ -46,30 +46,20 @@ implementation
 procedure TForm1.FormCreate(Sender: TObject);
 var i : integer;
 begin
-     v1 := vector.crear();
-     v2 := vector.crear();
-     v3 := vector.crear();
-     v4 := vector.crear();
-     v5 := vector.crear();
-     v := v1;
-     VEC[0] := SELF.v;
-     VEC[1] := SELF.v1;
-     VEC[2] := SELF.v2;
-     VEC[3] := SELF.v3;
-     VEC[4] := SELF.v4;
-     VEC[5] := SELF.v5;
      for i:=1 to 5 do begin
        ComboBox1.Items.Add('Vector' + IntToStr(i));
+       VEC[i] := vector.crear();
      end;
-
+     ComboBox1.ItemIndex := 0;
+     v := VEC[1];
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
+var a, b : integer;
 begin
-  v.CargarRnd(
-    StrToInt(InputBox('','rango A: ','1')),
-    StrToInt(InputBox('','rango B:','10'))
-  );
+  a := StrToInt(InputBox('','rango A: ','1'));
+  b := StrToInt(InputBox('','rango B:','10'));
+  v.CargarRnd(a, b);
   v.descargar(StringGrid1);
 end;
 
@@ -79,7 +69,7 @@ begin
 end;
 
 procedure TForm1.Button4Click(Sender: TObject);
- VAR N : integer;
+ //VAR N : integer;
 begin
   //v.ord_seleccion();
   //v.CargarMult2yMul3(v2,v3);
@@ -92,12 +82,13 @@ begin
      //CTRL + /
    //n := StrToInt(InputBox('','','')); //'12'=> 12
    //V.addAsc(n);
-   v5.FusionAsc(v1,v2,v3,v4);
+   //v.FusionAsc(vec[1], vec[2], vec[3],vec[4]);
+   v.RotarDerecha(StrToInt(InputBox('','','')));
 end;
 
 procedure TForm1.Button5Click(Sender: TObject);
 begin
-  v.RotarDerecha(StrToInt(InputBox('','','')));
+
 end;
 
 
@@ -113,7 +104,9 @@ procedure TForm1.ComboBox1Change(Sender: TObject);
 var pos: integer;
 begin
   pos := ComboBox1.ItemIndex;
-  v := self.VEC[pos];
+  {comentar rapido => ctrl + / }
+  ShowMessage(inttostr(pos));
+  v := self.VEC[pos+1];
   v.descargar(StringGrid1);
 end;
 
